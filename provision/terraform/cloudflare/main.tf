5df23eff7860d0300c508be30dd574f0dc9f9df6
prymalinstynct <zimzamfam@gmail.com>
2021-11-20 04:32:33 -0700

    enable flux to manage standalone splunk ingress

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -126,3 +126,12 @@ resource "cloudflare_record" "longhorn" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "splunk" {
+  name    = "splunk"
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  proxied = true
+  type    = "CNAME"
+  ttl     = 1
+}
