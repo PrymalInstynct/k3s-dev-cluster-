8cf5461aea4f0445cbb78f495d115b817e2c7038
prymalinstynct <zimzamfam@gmail.com>
2021-11-20 09:25:22 -0700

    commenting out some new stuff

--- scripts/initialize-cluster.sh
+++ scripts/initialize-cluster.sh
@@ -2,7 +2,7 @@
 task ansible:deps
 task ansible:playbook:ubuntu-prepare
 task ansible:playbook:k3s-install
-wait 6000
+sleep 300
 kubectl --kubeconfig=./provision/kubeconfig create namespace flux-system --dry-run=client -o yaml | kubectl --kubeconfig=./provision/kubeconfig apply -f -
 source .config.env
 gpg --export-secret-keys --armor "${BOOTSTRAP_FLUX_KEY_FP}" | kubectl --kubeconfig=./provision/kubeconfig create secret generic sops-gpg --namespace=flux-system --from-file=sops.asc=/dev/stdin
@@ -11,8 +11,6 @@ git commit -m "Reinitialize Commit - Part 1"
 git push
 kubectl --kubeconfig=./provision/kubeconfig apply --kustomize=./cluster/base/flux-system
 kubectl --kubeconfig=./provision/kubeconfig apply --kustomize=./cluster/base/flux-system
-wait 3000
-git add -A
-git commit -m "Reinitialize Commit - Part 2"
-git push
+sleep 90
+watch "task flux:k && echo && task flux:hr"
 exit 0
